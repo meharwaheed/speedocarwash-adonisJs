bfe9af415a89a46f72aaa1a171256ed903a99e7d
meharwaheed <meharwaheed4313@gmail.com>
2022-04-03 19:46:20 +0000

    initil commit

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,51 @@
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import User from "App/Models/User";
+import Database from "@ioc:Adonis/Lucid/Database";
+import { schema, rules } from '@ioc:Adonis/Core/Validator';
+export default class AuthController {
+  public async login({ request, auth }: HttpContextContract) {
+    const email = request.input("email");
+    const password = request.input("password");
+    const token = await auth.use("api").attempt(email, password, {
+      expiresIn: "10 days",
+    });
+    return token.toJSON();
+  }
+  public async register({ request, auth }: HttpContextContract) {
+  //  return await Database.rawQuery('drop table adonis_schema;');
+  //  Request validation
+    await this.validate(request);
+    
+    const email = request.input("email");
+    const password = request.input("password");
+    const name = request.input("name");
+    const newUser = new User();
+    newUser.email = email;
+    newUser.password = password;
+    newUser.name = name;
+    await newUser.save();
+    const token = await auth.use("api").login(newUser, {
+      expiresIn: "10 days",
+    });
+    return token.toJSON();
+  }
+
+  private async validate(request:object) {
+    await request.validate({
+      schema: schema.create({
+        name: schema.string({ trim: true }),
+        password: schema.string({ trim: true,}, [rules.minLength(5)]),
+        email: schema.string({ trim: true }, [
+          rules.unique({ table: 'users', column: 'email' })
+        ])    
+      })
+      messages: {
+        'name.required': '{{ field }} is required',
+        'email.unique': 'Account with this email is already exists',
+        'password.required': '{{ field }} is required',
+        'password.minLength': 'Password must be atleast 5 characters',
+
+      }
+    })
+  }
+}
